097ff34ef4447505864b7ac81879b4b02b2060ea
Simon Jacobs <simonadlerjacobs@gmail.com>
2021-01-13 18:52:42 -0500

    commit tree 29a497a5778cb51f4e0432a9b7ba156535f185e7

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,6 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :activate]
+  
   
   def index
     @students = Student.all
@@ -8,9 +9,18 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    @student.active = !@student.active
+    @student.save
+    # byebug
+    redirect_to student_path
+  end
+
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
+
+
 end
